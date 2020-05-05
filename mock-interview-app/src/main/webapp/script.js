8cd80fd7b951dc991e0906f2b9bbb168db5cf4e2
Juan Torres <juanditb@gmail.com>
2020-05-04 22:51:07 -0400

    Updated form. Times can no longer be in the past.

--- mock-interview-app/src/main/webapp/script.js
+++ mock-interview-app/src/main/webapp/script.js
@@ -188,13 +188,15 @@ async function getHeader() {
 
 var counter = 1;
 var limit = 10;
+let today = new Date().toISOString().substr(0,16);
 function addInput(divName){
      if (counter == limit)  {
           alert("You have reached the limit of adding " + counter + " time availability inputs");
      }
      else {
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='datetime-local' id='time_availability' name='time_availability' min='2020-01-01T00:00' max='2020-12-31T00:00'>";
+          newdiv.innerHTML = "Entry " + (counter + 1) + "<br><input type='datetime-local' class='form-control' id='time_availability' name='time_availability'>";
+          newdiv.innerHTML = `Entry ${counter+1} <br><input type='datetime-local' class='form-control' id='time_availability' name='time_availability' min='${today}'>`
           document.getElementById(divName).appendChild(newdiv);
           counter++;
      }
@@ -206,7 +208,13 @@ async function getAlert(){
      console.log(text);
      if(text.includes("1")){
         alert("You already have an interview coming up! Only can confirm one interview at a time.");
-        window.location.pathname = '/interviews.html';
-     }    
+        window.location.pathname = '/';
+     }
+     getMin();
 }
+
+function getMin() {
+  $("#time_availability").attr('min',today);
+}
+
 
